9814557e95e2439a9141024de6d6168c543c39e5
Luís Conrado <le.conrado3@gmail.com>
2025-05-27 19:57:46 -0300

    Add files via upload

---
+++ Exercicio-1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ImprimePadrao {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int n = scanner.nextInt();
+        
+        imprimirPadrao(n);
+    }
+
+    public static void imprimirPadrao(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
